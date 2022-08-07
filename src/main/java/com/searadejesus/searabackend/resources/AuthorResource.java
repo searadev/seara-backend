6270b9f29ab6a144c4047f08447d9ba1016fdf05
searadev <searadejesusdev@gmail.com>
2022-08-07 19:32:57 -0300

    ajustes

--- src/main/java/com/searadejesus/searabackend/resources/AuthorResource.java
+++ src/main/java/com/searadejesus/searabackend/resources/AuthorResource.java
@@ -1,25 +1,16 @@
 package com.searadejesus.searabackend.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
-import com.searadejesus.searabackend.dto.AuthorDTO;
-import com.searadejesus.searabackend.services.AuthorService;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.searadejesus.searabackend.dto.AuthorDTO;
+import com.searadejesus.searabackend.services.AuthorService;
 
 
 @RestController
